6dd10457511f28d265f8763a50a8212be5d98e3e
Thaysa <thay.fernnanda@gmail.com>
2022-07-01 15:46:53 -0300

    adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <main>
+          <p>TrybeTunes</p>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              component={ Login }
+            />
+            <Route
+              path="/search"
+              component={ Search }
+            />
+            <Route
+              path="/album/:id"
+              component={ Album }
+            />
+            <Route
+              path="/favorites"
+              component={ Favorites }
+            />
+            <Route
+              path="/profile/edit"
+              component={ ProfileEdit }
+            />
+            <Route
+              path="/profile"
+              component={ Profile }
+            />
+            <Route
+              path="/*"
+              component={ NotFound }
+            />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    );
   }
 }
 
