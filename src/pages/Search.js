175d5f24191d7d9aa281cb940d7df00774587490
Thaysa <thay.fernnanda@gmail.com>
2022-07-08 15:49:42 -0300

    adiciona requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,53 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      disabled: true,
+    };
+  }
+
+  isButtonDisabled = () => {
+    const { userName } = this.state;
+    if (userName.length >= 2) {
+      return this.setState({ disabled: false });
+    }
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name);
+    this.setState({ [name]: value }, () => {
+      this.isButtonDisabled();
+    });
+  };
+
   render() {
+    const { userName, disabled } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+      <>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="userName"
+            data-testid="search-artist-input"
+            value={ userName }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disabled }
+          >
+            Pesquisar
+          </button>
+        </div>
+      </>
     );
   }
 }
